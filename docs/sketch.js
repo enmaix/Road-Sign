1d733b89e9ba20255956317bfbceaebc773531b5
enmaix <aka.howl27@gmail.com>
2025-02-22 10:19:32 -0600

    Add files via upload

---
+++ docs/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(200,255,255);
+  fill ("grey")
+  rect(00,250,500,500)
+  fill("yellow")
+  strokeWeight(0)
+  rect(0,350,500,10);
+  rect(0,370,500,10);
+  fill("lightgrey");
+  stroke(0);
+  strokeWeight(1);
+  rect(130,150,25,100);
+  rect(345,150,25,100);
+  fill("white");
+  stroke(0);
+  strokeWeight(1);
+  rect(50,80,400,150,20);
+  fill("lavender");
+  stroke(0);
+  strokeWeight(1);
+  rect(60,90,380,130,10);
+  fill("white")
+  textSize(40);
+  stroke(0);
+  strokeWeight(3);
+  text("DRIFT YOUR CAR",80,170);
+  fill("black")
+  stroke(0);
+  strokeWeight(3);
+  textSize(100);
+  text("🏎️",135,455);
+  text("🏎️",225,315);
+}
+
